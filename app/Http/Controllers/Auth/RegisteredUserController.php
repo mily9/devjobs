511ba8c43fe3a4eaa2ccd6ec3848ef7b04d863d1
mily9 <emiliaashraghi@gmail.com>
2023-02-09 11:52:16 -0500

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,6 +34,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'rol' => ['required'],
         ]);
 
         $user = User::create([
